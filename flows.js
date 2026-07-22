581e2dd4bb9fbd8ecfde56d6ca47693f17e5f78a
Marvin Molijn <marvin@faqtic.co>
2026-07-22 18:43:14 +0200

    Revert target=_blank: Factorial sanitizes and strips target/rel from task links

--- flows.js
+++ flows.js
@@ -5,7 +5,7 @@ const FLOWS = {
   m: {
     cfgKey: 'manager_rates_hire',
     taskName: (d, emp) => `New Hire: Rate day ${d} of ${cfg.days_to_rate} - ${emp.full_name}`,
-    taskContent: (emp, link) => `How did ${emp.first_name}'s days go? <a href="${link}" target="_blank" rel="noopener">Fill in the ratings here</a> - you can complete several days at once.`,
+    taskContent: (emp, link) => `How did ${emp.first_name}'s days go? <a href="${link}">Fill in the ratings here</a> - you can complete several days at once.`,
     texts: (emp) => ({
       heading: `How did ${emp.full_name}'s days go?`,
       sub: `Rate ${emp.first_name}'s first week on behalf of the manager. You can fill in several days in one go.`,
@@ -15,7 +15,7 @@ const FLOWS = {
   h: {
     cfgKey: 'hire_rates_manager',
     taskName: (d, emp) => `First Week: Rate your manager, day ${d} of ${cfg.days_to_rate} - ${emp.full_name}`,
-    taskContent: (emp, link) => `How did the manager support ${emp.first_name} during the first week? <a href="${link}" target="_blank" rel="noopener">Fill in the ratings here</a> - you can complete several days at once.`,
+    taskContent: (emp, link) => `How did the manager support ${emp.first_name} during the first week? <a href="${link}">Fill in the ratings here</a> - you can complete several days at once.`,
     texts: (emp) => ({
       heading: 'How did the manager support the new hire?',
       sub: `Record ${emp.first_name}'s feedback about their manager, day by day. You can fill in several days in one go.`,
